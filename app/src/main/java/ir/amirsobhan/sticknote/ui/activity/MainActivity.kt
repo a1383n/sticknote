2d2fa3ca1ce0e84f39804b189506f2ec0d1e2708
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2021-03-02 23:26:52 +0330

    Replace viewPager with Navigation

--- app/src/main/java/ir/amirsobhan/sticknote/ui/activity/MainActivity.kt
+++ app/src/main/java/ir/amirsobhan/sticknote/ui/activity/MainActivity.kt
@@ -3,10 +3,11 @@ package ir.amirsobhan.sticknote.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import ir.amirsobhan.sticknote.R
-import ir.amirsobhan.sticknote.adapters.MainViewPagerAdapter
 import ir.amirsobhan.sticknote.databinding.ActivityMainBinding
 
 
@@ -24,33 +25,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setupBottomNavigation(){
-        activityMainBinding.viewPager.adapter = MainViewPagerAdapter(
-            supportFragmentManager,
-            lifecycle
-        )
-        activityMainBinding.viewPager.currentItem = 2
-        activityMainBinding.bottomNavigation.selectedItemId = R.id.notes
-        activityMainBinding.viewPager.isUserInputEnabled = false;
-        activityMainBinding.bottomNavigation.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.setting -> activityMainBinding.viewPager.currentItem = 0
-                R.id.cloud -> activityMainBinding.viewPager.currentItem = 1
-                R.id.notes -> activityMainBinding.viewPager.currentItem = 2
-                R.id.info -> activityMainBinding.viewPager.currentItem = 3
-                else -> false
-            }
-            true
-        }
+    private fun setupBottomNavigation() {
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        NavigationUI.setupWithNavController(activityMainBinding.bottomNavigation, navHostFragment!!.navController)
+
 
         // Add Corner to BottomBar
         var radius = resources.getDimension(R.dimen.bottom_nav_corner)
         val bottomBarBackground = activityMainBinding.bottomBar.background as MaterialShapeDrawable
         bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel
             .toBuilder()
-            .setTopRightCorner(CornerFamily.ROUNDED,radius)
-            .setTopLeftCorner(CornerFamily.ROUNDED,radius)
+            .setTopRightCorner(CornerFamily.ROUNDED, radius)
+            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
             .build()
-
     }
 }

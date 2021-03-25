a3c28aec6873fec2e8a433db00a8e4a203bf31a7
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2021-03-25 10:36:15 +0430

    RegisterFragment.kt almost completed

--- app/src/main/java/ir/amirsobhan/sticknote/ui/fragments/auth/RegisterFragment.kt
+++ app/src/main/java/ir/amirsobhan/sticknote/ui/fragments/auth/RegisterFragment.kt
@@ -7,28 +7,43 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
 import ir.amirsobhan.sticknote.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
     private var _binding : FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentRegisterBinding.inflate(layoutInflater,container,false)
-
-
         init()
+
+        binding.registerButton.setOnClickListener {
+            if (formValidation()) {
+                val userProfile = UserProfileChangeRequest.Builder()
+                        .setDisplayName(binding.inputName.text.toString())
+                        .build()
+
+                auth.createUserWithEmailAndPassword(binding.inputEmail.text.toString(),binding.passwordInput.text.toString())
+                        .addOnSuccessListener { it.user.updateProfile(userProfile) }
+                        .addOnFailureListener { binding.registerError.apply {
+                            visibility = View.VISIBLE
+                            text = it.message
+                        }}
+            }
+        }
 
 
         return binding.root
     }
 
     fun init(){
-        binding.registerButton.setOnClickListener {
-            if (formValidation()){
-
-            }
-        }
+        auth = Firebase.auth
     }
 
     fun formValidation() : Boolean{
